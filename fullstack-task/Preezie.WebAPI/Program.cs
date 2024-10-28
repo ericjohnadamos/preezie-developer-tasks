72482d87bec6fb0dba06f15832da685d7a2395dc
Eric John Adamos <ericjohnadamos@gmail.com>
2024-10-29 01:45:12 +0800

    Add swagger / open api for easy access and QOL

--- fullstack-task/Preezie.WebAPI/Program.cs
+++ fullstack-task/Preezie.WebAPI/Program.cs
@@ -4,8 +4,16 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.MapControllers();
 
 app.Run();
