cc508afdbbba64e74ce99cef22f4a5d3fc76a8a2
Eric John Adamos <ericjohnadamos@gmail.com>
2024-10-29 01:04:55 +0800

    Initial commit for backend API

---
+++ fullstack-task/Preezie.WebAPI/Controllers/TodoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+[ApiController]
+[Route("api/todo")]
+public class TodoController : ControllerBase
+{
+    public static List<TodoItem> todos = new List<TodoItem>();
+    public int nextId = 1;
+
+    [HttpGet]
+    public IEnumerable<TodoItem> GetTodos()
+    {
+        return todos;
+    }
+
+    [HttpPost]
+    public void AddTodo(TodoItem item)
+    {
+        item.Id = nextId;
+        nextId++;
+        todos.Add(item);
+    }
+
+    [HttpPut("{id}")]
+    public void UpdateTodo(int id)
+    {
+        var todo = todos.FirstOrDefault(x => x.Id == id);
+        if (todo != null)
+        {
+            todo.IsCompleted = !todo.IsCompleted;
+        }
+    }
+
+    [HttpDelete]
+    public void DeleteTodo(int id)
+    {
+        var todo = todos.FirstOrDefault(x => x.Id == id);
+        if (todo != null)
+        {
+            todos.Remove(todo);
+        }
+    }
+}
