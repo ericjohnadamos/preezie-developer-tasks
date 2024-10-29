2770a1ae6e474d40f41578241b58b249a924b3d2
Eric John Adamos <ericjohnadamos@gmail.com>
2024-10-29 16:22:44 +0800

    Create a domain entity `TodoItem`

---
+++ fullstack-task/Preezie.Domain/Entities/TodoItem.cs
@@ -0,0 +1,34 @@
+﻿namespace Preezie.Domain.Entities;
+
+public class TodoItem
+{
+    public int Id { get; private set; }
+    public string Title { get; private set; } = string.Empty;
+    public bool IsCompleted { get; private set; }
+
+    private TodoItem() { }
+
+    public static TodoItem CreateWithTitle(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new Exception("Title is required.");
+
+        return new TodoItem
+        {
+            Title = title,
+        };
+    }
+
+    public void UpdateTitle(string newTitle)
+    {
+        if (string.IsNullOrWhiteSpace(Title))
+            throw new Exception("Title is required.");
+
+        Title = newTitle;
+    }
+
+    public void MarkAsCompleted()
+    {
+        IsCompleted = true;
+    }
+}
